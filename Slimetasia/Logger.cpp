65d929184bcfc8d4b8f178afc59cf486803cc3f6
JasonWyx <jasonwangyuxuan@hotmail.com>
2022-03-12 17:50:42 +0800

    Update assert log format - Changed logs to write to the same file rather than new logs all the time

--- Slimetasia/Logger.cpp
+++ Slimetasia/Logger.cpp
@@ -4,26 +4,25 @@ void Logger::LogAssert(bool expr, const char* cpp, const char* func, long line)
 {
     if (!expr)
     {
-        int result = _mkdir("LOG");
+        int result = _mkdir("Logs");
 
-        char buf[100];
-        std::ofstream myfile;
         time_t now = time(0);
-        std::string filename = "LOG\\ASSERT_LOG_";
-        ctime_s(buf, sizeof(buf), &now);
-        std::string clock = buf;
-        std::replace(clock.begin(), clock.end(), ':', '_');
-        clock.pop_back();
-        filename += clock;
-        filename += ".txt";
-        myfile.open(filename);
-        if (myfile.is_open())
+
+        char timeStamp[100];
+        ctime_s(timeStamp, sizeof(timeStamp), &now);
+
+        std::ofstream outFile;
+        std::string filename = "Logs\\ASSERT_LOG.txt";
+
+        std::string timeStampString = timeStamp;
+
+        outFile.open(filename);
+        if (outFile.is_open())
         {
-            myfile << "File Name: " << cpp << std::endl;
-            myfile << "Function Name: " << func << std::endl;
-            myfile << "Line Number: " << line << std::endl;
+            outFile << timeStampString << ":" << cpp << " " << func << ":" << line << std::endl;
         }
-        myfile.close();
+        outFile.close();
+
         assert(expr);
     }
 }
--- Slimetasia/Logger.h
+++ Slimetasia/Logger.h
@@ -8,7 +8,7 @@
 #include <iostream>
 
 #ifdef ENABLE_ASSERTS
-#define ASSERT(expr) Logger::LogAssert((!!(expr)), __FILE__, __func__, __LINE__)
+#define ASSERT(expr) Logger::LogAssert(expr, __FILE__, __func__, __LINE__)
 #else
 #define ASSERT(expr) true
 #endif
